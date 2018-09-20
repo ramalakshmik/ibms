f8bb258ae938df20b0be272e417b624c313358ab
sys-user <sys-user@SMI-131>
2018-09-20 18:37:42 +0530

    Added classes for ExceptionHandling

---
+++ src/main/java/com/aruvi/ibms/exception/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.aruvi.ibms.exception;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName(value = "error")
+public class ErrorResponse {
+	public ErrorResponse(String message, List<String> details) {
+		super();
+		this.message = message;
+		this.details = details;
+	}
+
+	// General error message about nature of error
+	private String message;
+
+	// Specific errors in API request processing
+	private List<String> details;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<String> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}
+
+}
